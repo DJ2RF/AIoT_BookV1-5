98ce2dcd1af28114c4f1ed82edeab10187b14114
dj2rf <dj2rf@darc.de>
2026-02-10 09:06:21 +0100

    Added AIoT_I2C_Test

---
+++ software/Book1/AIoT_I2C_Test/main/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/i2c.h"
+#include "esp_log.h"
+
+#define I2C_MASTER_SCL_IO 9
+#define I2C_MASTER_SDA_IO 8
+#define I2C_MASTER_NUM I2C_NUM_0
+#define I2C_MASTER_FREQ_HZ 100000
+
+static const char *TAG = "PROJECT13";
+
+void i2c_master_init()
+{
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ,
+    };
+
+    i2c_param_config(I2C_MASTER_NUM, &conf);
+    i2c_driver_install(I2C_MASTER_NUM, conf.mode, 0, 0, 0);
+}
+
+void i2c_scanner()
+{
+    ESP_LOGI(TAG, "I2C Scan gestartet");
+
+    for (uint8_t addr = 1; addr < 127; addr++)
+    {
+        i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+        i2c_master_start(cmd);
+        i2c_master_write_byte(cmd, (addr << 1) | I2C_MASTER_WRITE, true);
+        i2c_master_stop(cmd);
+
+        esp_err_t ret = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, pdMS_TO_TICKS(50));
+        i2c_cmd_link_delete(cmd);
+
+        if (ret == ESP_OK)
+        {
+            ESP_LOGI(TAG, "Gerät gefunden bei Adresse: 0x%02X", addr);
+        }
+    }
+}
+
+void app_main(void)
+{
+    i2c_master_init();
+
+    while (1)
+    {
+        i2c_scanner();
+        vTaskDelay(pdMS_TO_TICKS(5000));
+    }
+}
